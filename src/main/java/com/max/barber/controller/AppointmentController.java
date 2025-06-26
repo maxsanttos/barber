183e088e98162b980135467fd213295904d7b35a
rimuru <maxsanttos@hotmail.com>
2025-06-26 14:48:57 -0300

    creating methods controller

--- src/main/java/com/max/barber/controller/AppointmentController.java
+++ src/main/java/com/max/barber/controller/AppointmentController.java
@@ -1,10 +1,49 @@
 package com.max.barber.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.max.barber.model.services.Appointment;
+import com.max.barber.service.AppointmentService;
 
 @RestController
 @RequestMapping("/appointments")
 public class AppointmentController {
     
+     @Autowired
+    private AppointmentService appointmentService;
+
+    // Listar todos os agendamentos
+    @GetMapping
+    public ResponseEntity<List<Appointment>> getAllAppointments() {
+        List<Appointment> appointments = appointmentService.getAllAppointments();
+        return ResponseEntity.ok(appointments);
+    }
+
+    // Buscar agendamento por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Appointment> getAppointmentById(@PathVariable Long id) {
+        Appointment appointment = appointmentService.getAppointmentById(id);
+        return ResponseEntity.ok(appointment);
+    }
+
+    // Atualizar agendamento
+    @PutMapping("/{id}")
+    public ResponseEntity<Appointment> updateAppointment(@PathVariable Long id, @RequestBody Appointment updated) {
+        Appointment appointment = appointmentService.updateAppointment(id, updated);
+        return ResponseEntity.ok(appointment);
+    }
+
+    // Deletar agendamento
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAppointment(@PathVariable Long id) {
+        appointmentService.deleteAppointment(id);
+        return ResponseEntity.noContent().build();
+    }
 }
